2d2e6b91a0dd56289228ecfc698fd6bdb0f1b76f
David LeClercq <dleclercq289@gmail.com>
2021-07-24 15:28:16 -0400

    last commit

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 const baseURL = "https://calendarific.com/api/v2/countries/ISO&3166-2:US/holidays";
-const key = "cb105ee55b5f128fe2b113d4ca0d37ae44665d81";
+const key = "66fe0e2ac9dcc450ac217d3ff238c82c20d70d91";
 
 fetch (`${baseURL}?api_key=${key}`)
 .then(response => response.json())
 .then(json => calendar(json));
 
-function calendar(holidays) {
-    console.log(holidays);
+let div = document.getElementById("divPlease");
+let title = document.createElement("h1");
+title.innerText = "Country Randomizer"
+title.style.textAlign = "center";
+title.style.fontSize = "48px";
+title.style.position = "absolute";
+title.style.left = "37%";
+title.style.top = "35%";
+title.style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif"
 
-    let div = document.getElementById("divPlease");
+function calendar(holidays) {
     let display = document.createElement("h1");
-    let title = document.createElement("h1");
-    title.innerText = "Country Randomizer"
-    title.style.textAlign = "center";
-    title.style.fontSize = "48px";
-    title.style.position = "absolute";
-    title.style.left = "37%";
-    title.style.top = "35%";
-    title.style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif"
-    
-    // holidays.response.countries[0].country_name
-    for(let n of holidays.response.countries) {
-        n = Math.floor(Math.random() * 230); // generate country
-        display.innerText = country_name[n]; // assign country to html
-          display.style.textAlign = "center";
-          display.style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
-            display.style.position = "absolute";
-            display.style.left = "45%";
-            display.style.top = "47%";
-        }
-      div.appendChild(title);
+
+    for(let i = 0; i < 1; i++) {
+      let n = Math.floor(Math.random() * 230); // generate country
+      display.innerText = holidays.response.countries[n].country_name;
+
+      display.style.textAlign = "center";
+      display.style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
+      display.style.position = "absolute";
+      display.style.left = "45%";
+      display.style.top = "47%";
+    }
       div.appendChild(display);
 }
+div.appendChild(title);
